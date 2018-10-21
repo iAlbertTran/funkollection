13c64fd7bc4a1956ef8c6b9609f7c5e6d3d3242e
Albert <albtran@ucdavis.edu>
2018-10-20 18:58:07 -0700

    added get request to get series names that exist in database

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -7,16 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  imageSrc: string;
- 
+  imageSrc: any;
+  event: any;
+  url = "http://localhost:8000/api/series";
+  seriesNames: any;
   constructor() { }
 
   ngOnInit() {
+    var oReq = new XMLHttpRequest();
+    oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", this.url);
+
+    oReq.send();
+
+    function reqListener(){
+      this.seriesNames = JSON.parse(oReq.responseText);
+      this.seriesNames = this.seriesNames.sort(function(a,b){
+        return (b["SERIESNAME"] < a["SERIESNAME"]) ? 1 : ((b["SERIESNAME"] > a["SERIESNAME"]) ? -1 : 0);
+      });
+      console.log(this.seriesNames);
+    }
   }
 
+
   readURL(event: Event): void {
-    if (event.target.files && event.target.files[0]) {
-        const file = event.target.files[0];
+    this.event = event;
+    if (this.event.target.files && this.event.target.files[0]) {
+        const file = this.event.target.files[0];
 
         const reader = new FileReader();
         reader.onload = e => this.imageSrc = reader.result;
