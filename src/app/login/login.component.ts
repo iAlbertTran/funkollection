1372dea97bca94fd1848073d2d9c39fa5510a49c
Albert Tran <albtran@ucdavis.edu>
2018-11-06 18:39:59 -0800

    added login api call to log in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router }      from '@angular/router';
+import { LoginModel } from '../models/loginModel';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginModel]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, public router: Router, private _loginModel: LoginModel) { }
 
+  username: string;
+  password: string;
+
+  loginFailed: boolean = false;
+  
   ngOnInit() {
+  }
+
+  login() {
+    this._loginModel.username = this.username;
+    this._loginModel.password = this.password;
+
+    this.authService.login(this._loginModel);
+    console.log(this.authService.isLoggednIn);
+    if (this.authService.isLoggednIn()) {
+      this.loginFailed = false;
+      this.router.navigate([""]);
+    }
+    else{
+      this.loginFailed = true;
+    }
+  }
+ 
+  logout() {
+    this.authService.logout();
   }
 
 }
