707ed20bc40e26964a6fc8117b42df42256d1917
Albert Tran <albtran@ucdavis.edu>
2018-11-08 01:48:14 -0800

    changed category input to selector. Prepped upload component for api implementation

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -5,6 +5,7 @@ import { FunkollectionApiService } from '../services/funkollection-api.service';
 import { map } from "rxjs/operators";
 
 import { Series } from "../models/Series";
+import { Category } from "../models/category";
 
 @Component({
   selector: 'upload',
@@ -16,13 +17,27 @@ export class UploadComponent implements OnInit {
 
   imageSrc: any;
   event: any;
+
   series: Series[];
-  displayNewSeriesInput: boolean;
+  categories: Category[];
+
+  displayNewSeriesInput: boolean = false;
+  displayNewCategoryInput: boolean = false;
+
+  name: string;
+  seriesSelector: string;
+  seriesInput: string;
+  categorySelector: string;
+  categoryInput: string;
+  number: string;
+
+
   constructor( private apiService: FunkollectionApiService ) { 
   }
 
   ngOnInit() {
     this.getSeries();
+    this.getCategories();
     this.displayNewSeriesInput = false;
   }
 
@@ -32,7 +47,22 @@ export class UploadComponent implements OnInit {
         series.sort(function (a, b) {
           return a.name.localeCompare(b.name);
       });
+
         this.series = series;
+
+      });
+    
+
+  }
+
+  getCategories(): void{
+    this.apiService.getCategories()
+      .subscribe(categories => {
+        categories.sort(function (a, b) {
+          return a.name.localeCompare(b.name);
+      });
+        console.log(categories);
+        this.categories = categories;
 
       });
     
@@ -52,7 +82,7 @@ export class UploadComponent implements OnInit {
     }
   }
 
-  checkSelection(value: any){
+  checkSeriesSelection(value: any){
 
     if(value == 'Other'){
       this.displayNewSeriesInput = true;
@@ -60,5 +90,21 @@ export class UploadComponent implements OnInit {
     else{
       this.displayNewSeriesInput = false;
     }
+  }
+
+  checkCategorySelection(value: any){
+
+    if(value == 'Other'){
+      this.displayNewCategoryInput = true;
+    }
+    else{
+      this.displayNewCategoryInput = false;
+    }
+  }
+
+  uploadPop(){
+    console.log(this.name);
+    console.log(this.number);
+    console.log(this.seriesSelector == undefined);
   }
 }
