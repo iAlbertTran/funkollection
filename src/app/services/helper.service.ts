20ee60b80b053ad7638bb8dab916a64053184293
Albert Tran <albtran@ucdavis.edu>
2018-11-12 02:09:37 -0800

    helper service that sets error pop

---
+++ src/app/services/helper.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class HelperService {
+
+    errorOccured: boolean = false;
+    errorText: string = "";
+    
+    errorMessages = [];
+    ngOnInit(){
+    }
+
+    addErrorToMessages(msg: String){
+        var index = this.errorMessages.indexOf(msg);
+
+        if (index == -1) {
+            this.errorMessages.push(msg);
+        }
+
+        this.showErrorMessages();
+
+    }
+
+    removeErrorFromMessages(msg: String){
+
+        var index = this.errorMessages.indexOf(msg);
+
+        if (index > -1) {
+        this.errorMessages.splice(index, 1);
+        }
+
+        this.showErrorMessages();
+    }
+  
+    showErrorMessages(){
+
+        if(this.errorMessages.length > 0){
+            this.errorText = "";
+            this.errorMessages.forEach(element => {
+                this.errorText += element + " ";
+            });
+
+            this.errorOccured = true;
+        }
+        else{
+            this.hideErrorMessages();
+        }
+    }
+
+    hideErrorMessages(){
+        this.errorOccured = false;
+    }
+
+    removeAllErrors(){
+        this.errorMessages = [];
+        this.hideErrorMessages();
+    }
+
+    
+}
