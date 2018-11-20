e735d1ddd0482bea44673eec27df890e7d7ed694
Albert <albtran@ucdavis.edu>
2018-11-19 19:31:22 -0800

    added more fields to funko pop model

--- src/app/models/funkopop.ts
+++ src/app/models/funkopop.ts
@@ -1,9 +1,11 @@
-import { Series } from "../models/Series";
+import { Series } from "../models/series";
 import { Category } from "../models/category";
 
 export class FunkoPop {
+    id: string;
     name: string;
     series: Series;
     category: Category;
     number: number;
+    image: string;
   }
