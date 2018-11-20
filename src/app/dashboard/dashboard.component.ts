32ec8727ed0e774eb294a0fbedc7b0576b74bad2
Albert <albtran@ucdavis.edu>
2018-11-19 19:31:12 -0800

    initial call for all funkopops and displaying them on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FunkollectionApiService } from '../services/funkollection-api.service';
+import { HelperService } from '../services/helper.service';
+import { FunkoPop } from '../models/funkopop';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  getFunkoPopFailedMessage: String = 'Unable to retrieve Pop! Vinyls';
+  funkopops: FunkoPop[];
+
+  constructor(private apiService: FunkollectionApiService, private _helperService: HelperService) { }
 
   ngOnInit() {
+    this.getUserPops();
   }
 
+  getUserPops(){
+    this.apiService.getAllFunkoPops()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            var popArray = res['funkopops'];
+            this.funkopops = popArray;
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
 }
