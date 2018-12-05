7451de88c55501a274a237ffd16984c200928850
Albert <albtran@ucdavis.edu>
2018-12-04 20:03:20 -0800

    display funkopops in a grid with overlay

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FunkollectionApiService } from '../services/funkollection-api.service';
 import { HelperService } from '../services/helper.service';
 import { FunkoPop } from '../models/funkopop';
@@ -25,7 +25,11 @@ export class DashboardComponent implements OnInit {
         res => { 
           if(res['statusCode'] == 200){
             var popArray = res['funkopops'];
+            popArray.sort((a,b) => {
+              return a.series.localeCompare(b.series) || a.category.localeCompare(b.category) || a.name.localeCompare(b.name)|| a.number - b.number;
+            });
             this.funkopops = popArray;
+            console.log(this.funkopops);
           }
         },
         err => {
@@ -33,5 +37,17 @@ export class DashboardComponent implements OnInit {
         }
 
       );
+  }
+
+  showOverlay(id: string){
+    let popOverlay = document.getElementById(id + '-pop-overlay');
+
+    popOverlay.className="pop-overlay opacity";
+  }
+
+  hideOverlay(id: string){
+    let popOverlay = document.getElementById(id + '-pop-overlay');
+
+    popOverlay.className="pop-overlay";
   }
 }
