b844277cd51eeb09613646b9381f1b981e04f519
Albert <albtran@ucdavis.edu>
2018-12-13 03:16:52 -0800

    setup for info display

--- src/app/funkopop/funkopop.component.ts
+++ src/app/funkopop/funkopop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { FunkollectionApiService } from '../services/funkollection-api.service';
+import { HelperService } from '../services/helper.service';
 
 
 @Component({
@@ -12,8 +13,10 @@ export class FunkopopComponent implements OnInit {
   series: string;
   category: string;
   name: string;
-
-  constructor(private route: ActivatedRoute, private apiService: FunkollectionApiService) { }
+  basicInfo = null;
+  getFunkoPopFailedMessage: string = 'Unable to retrieve information on Pop! Vinyl.';
+  
+  constructor(private route: ActivatedRoute, private apiService: FunkollectionApiService, private _helperService: HelperService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -29,11 +32,15 @@ export class FunkopopComponent implements OnInit {
     this.apiService.getFunkoPop(this.series, this.category, this.name).subscribe(
       res => { 
         if(res['statusCode'] == 200){
-          console.log('success!');
+          this.basicInfo = res['funkopop'];
+
+          if(this.basicInfo == null){
+            this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+          }
         }
       },
       err => {
-        //this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
       }
 
     );
