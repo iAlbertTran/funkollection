ad1dbc47e8f49a8a7f974766841c1f66b1f6d203
Albert Tran <albtran@ucdavis.edu>
2018-12-15 02:14:09 -0800

    api calls for collection, wishlist, and pops in series

--- src/app/funkopop/funkopop.component.ts
+++ src/app/funkopop/funkopop.component.ts
@@ -10,11 +10,20 @@ import { HelperService } from '../services/helper.service';
   styleUrls: ['./funkopop.component.css']
 })
 export class FunkopopComponent implements OnInit {
+
   series: string;
   category: string;
   name: string;
   basicInfo = null;
+
+  collection = [];
+  wishlist = [];
+  seriespops = null;
+  categorypops = null;
+
   getFunkoPopFailedMessage: string = 'Unable to retrieve information on Pop! Vinyl.';
+  addFailedMessage: String = 'Unable to add Pop! Vinyl to your';
+  removeFailedMessage: String = 'Unable to remove Pop! Vinyl from your';
   
   constructor(private route: ActivatedRoute, private apiService: FunkollectionApiService, private _helperService: HelperService) { }
 
@@ -25,6 +34,8 @@ export class FunkopopComponent implements OnInit {
       this.name = params.get("name");
 
       this.getInfo();
+      this.getUserCollection();
+      this.getUserWishlist();
     })
   }
 
@@ -33,14 +44,146 @@ export class FunkopopComponent implements OnInit {
       res => { 
         if(res['statusCode'] == 200){
           this.basicInfo = res['funkopop'];
-
+          
           if(this.basicInfo == null){
             this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+          }
+          else{
+            this.getPopsInSeries(this.basicInfo.series);
           }
         }
       },
       err => {
         this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+      }
+
+    );
+  }
+
+  getUserCollection(){
+    this.apiService.getUserCollection()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            this.collection = res['funkopops'];
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
+
+  getUserWishlist(){
+    this.apiService.getUserWishlist()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            this.wishlist = res['funkopops'];
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
+
+  getPopsInSeries(series: string){
+    console.log(series);
+    this.apiService.getPopsInSeries(series)
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            this.seriespops = res['funkopops'].filter( element => {
+              return element.name != this.basicInfo.name;
+            });
+          }
+        },
+        err => {
+        }
+
+      );
+  }
+
+  addToCollection(id: string){
+    $(`#${id}-collection-button`).addClass('animated faster pulse');
+
+    setTimeout(() => {
+      $(`#${id}-collection-button`).removeClass('animated faster pulse');
+    }, 500);
+
+    if(this.collection.includes(id)){
+      this.removeFromCollection(id);
+    }
+
+    else{
+
+      this.apiService.addToCollection(id).subscribe(
+        res => { 
+          this.getUserCollection();
+        },
+        err => {
+          this.getUserCollection();
+          this._helperService.addErrorToMessages(`${this.addFailedMessage} collection.`);
+        }
+
+      );
+    }
+  }
+
+  removeFromCollection(id: string){
+
+    this.apiService.removeFromCollection(id).subscribe(
+      res => { 
+        this.getUserCollection();
+
+      },
+      err => {
+        this.getUserCollection();
+        this._helperService.addErrorToMessages(`${this.removeFailedMessage} collection.`);
+      }
+
+    );
+  }
+
+  addToWishlist(id: string){
+    $(`#${id}-wishlist-button`).addClass('animated faster pulse');
+
+    setTimeout(() => {
+      $(`#${id}-wishlist-button`).removeClass('animated faster pulse');
+    }, 500);
+
+    if(this.wishlist.includes(id)){
+      this.removeFromWishlist(id);
+    }
+
+    else{
+
+      this.apiService.addToWishlist(id).subscribe(
+        res => { 
+          this.getUserWishlist();
+        },
+        err => {
+          this.getUserWishlist();
+          this._helperService.addErrorToMessages(`${this.removeFailedMessage} wishlist.`);
+        }
+
+      );
+    }
+  }
+
+  removeFromWishlist(id: string){
+
+    this.apiService.removeFromWishlist(id).subscribe(
+      res => { 
+        this.getUserWishlist();
+
+      },
+      err => {
+        this.getUserWishlist();
+        this._helperService.addErrorToMessages(`${this.removeFailedMessage} wishlist.`);
       }
 
     );
