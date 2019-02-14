5d1a512f209729d020ed307f6f327471c481a8a2
A-Mac <albtran@ucdavis.edu>
2019-02-13 17:39:08 -0800

    added content to wishlist / collection components similar to dashboard

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }      from '@angular/router';
+import { FunkollectionApiService } from '../services/funkollection-api.service';
+import { HelperService } from '../services/helper.service';
+import { FunkoPop } from '../models/funkopop';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-collection',
@@ -7,9 +12,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionComponent implements OnInit {
 
-  constructor() { }
+  getFunkoPopFailedMessage: string = 'Unable to retrieve any Pop! Vinyls ';
+  addFailedMessage: String = 'Unable to add ';
+  removeFailedMessage: String = 'Unable to remove ';
+  addSuccess: String = 'Successfully added ';
+  removeSuccess: String = 'Successfully removed ';
+
+  collection = [];
+  wishlist = [];
+
+  constructor(public router: Router, private apiService: FunkollectionApiService, private _helperService: HelperService) { }
 
   ngOnInit() {
+    this.getUserCollection();
+    this.getUserWishlist();
   }
 
+
+  getUserCollection(){
+    this.apiService.getUserCollection()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            let pops = res['funkopops'];
+            let size = 0;
+
+            setInterval(() =>{
+              if(size == pops.length)
+                return;
+              this.collection.push(pops[size]);
+              ++size;
+            }, 50);
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
+
+  getUserWishlist(){
+    this.apiService.getUserWishlistID()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            this.wishlist = res['funkopops'];
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
+  /*
+  sortBySeries(popArray: FunkoPop[]){
+    
+    while(popArray != null && popArray.length > 0){
+      let popJSON = {};
+      let series = popArray[0].series;
+      let popsInSeries = popArray.filter((pop) => {
+        return pop.series == series;
+      });
+      
+      let popCount = popsInSeries.length;
+      popArray.splice(0, popCount);
+      popJSON['seriesName'] = series;
+      popJSON['popList'] = popsInSeries;
+      
+      this.funkopops.push(popJSON);
+    }
+
+  }
+  */
 }
--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }      from '@angular/router';
+import { FunkollectionApiService } from '../services/funkollection-api.service';
+import { HelperService } from '../services/helper.service';
+import { FunkoPop } from '../models/funkopop';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() { }
+  getFunkoPopFailedMessage: string = 'Unable to retrieve any Pop! Vinyls ';
+  addFailedMessage: String = 'Unable to add ';
+  removeFailedMessage: String = 'Unable to remove ';
+  addSuccess: String = 'Successfully added ';
+  removeSuccess: String = 'Successfully removed ';
+
+  collection = [];
+  wishlist = [];
+
+  constructor(public router: Router, private apiService: FunkollectionApiService, private _helperService: HelperService) { }
 
   ngOnInit() {
+    this.getUserCollection();
+    this.getUserWishlist();
+  }
+
+  getUserCollection(){
+    this.apiService.getUserCollectionID()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            this.collection = res['funkopops'];
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
+  }
+
+  getUserWishlist(){
+    this.apiService.getUserWishlist()
+      .subscribe(
+        res => { 
+          if(res['statusCode'] == 200){
+            let pops = res['funkopops'];
+            let size = 0;
+
+            setInterval(() =>{
+              if(size == pops.length)
+                return;
+              this.wishlist.push(pops[size]);
+              ++size;
+            }, 50);
+          }
+        },
+        err => {
+          this._helperService.addErrorToMessages(this.getFunkoPopFailedMessage);
+        }
+
+      );
   }
 
 }
